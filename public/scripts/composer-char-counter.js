034ee3b3f4a3f4297558f5e0c01273a028a915a6
Dan Pletzke <dpletzke@gmail.com>
2020-10-06 20:07:51 +0000

    tweet char counter with css coloring when negative working

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  $('#tweet-text').on('input', function (event) {
+    const counter = $(this).siblings('div').children('output');
+    counter.text(140 - this.value.length);
+    if(Number(counter.text()) < 0) {
+      counter.addClass('counter-negative');
+    } else {
+      counter.removeClass('counter-negative');
+    }
+  });
+});
