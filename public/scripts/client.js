70dc446cfc42cd53e9b94071683169e1106f33cf
Dan Pletzke <dpletzke@gmail.com>
2020-10-07 22:41:40 +0000

    add cient js helpers to dynamically add tweets from db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,40 +4,40 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const parseISODateTime = (dateTime) => {
+const parseDate = (date) => {
+  const dateTime = date.toISOString();
   const time = {
-    y: dateTime.slice(0, 5),
-    m: dateTime.slice(6, 8),
-    d: dateTime.slice(9, 11),
-    h: dateTime.slice(12, 14),
-    min: dateTime.slice(15, 17),
-    s: dateTime.slice(18, 20),
-    ms: dateTime.slice(21, 24)
-  }
-  for (t in time) {
+    year: dateTime.slice(0, 4),
+    month: dateTime.slice(5, 7),
+    day: dateTime.slice(8, 10),
+    hour: dateTime.slice(11, 13),
+    minute: dateTime.slice(14, 16),
+    second: dateTime.slice(17, 19),
+    millisecond: dateTime.slice(20, 23)
+  };
+  for (const t in time) {
     time[t] = Number(time[t]);
   }
  
   return time;
-}
+};
 
 const getTimeAgo = (milliseconds) => {
-  const currentTime = parseISODateTime(new Date().toISOString());
-  const timePostedISO = (new Date(0)).setUTCSeconds(milliseconds).toISOString();
-  const timePosted = parseISODateTime(timePostedISO);
+
+  const currentTime = parseDate((new Date()));
+  const timePosted = parseDate(new Date(milliseconds));
 
   const timeRef = Object.keys(currentTime);
   const displayKey = timeRef.find(t => currentTime[t] !== timePosted[t]);
   const diffTime = currentTime[displayKey] - timePosted[displayKey];
-  return `${diffTime} ${displayKey}${diffTime !== 1 ? '' : 's'} ago`; 
-}
+  return `${diffTime} ${displayKey}${diffTime !== 1 ? 's' : ''} ago`;
+};
 
 const createTweetElement = (tweet) => {
   const { user, content, created_at } = tweet;
   const { name, avatars, handle } = user;
-
-  const exactTime = new Date(created_at);
-  const timeAgo = getTimeAgo(created_at);
+  const timePosted = new Date(0);
+  timePosted.setUTCMilliseconds(created_at);
 
   let $tweet = '';
   $tweet += `<article>`;
@@ -48,7 +48,7 @@ const createTweetElement = (tweet) => {
   $tweet += `</header>`;
   $tweet += `<p>${content.text}</p>`;
   $tweet += `<footer>`;
-  $tweet += `<p title="${exactTime}">${timeAgo}</p>`;
+  $tweet += `<p title="${timePosted}">${getTimeAgo(created_at)}</p>`;
   $tweet += `<div>`;
   $tweet += `<a class="fa fa-tripadvisor" aria-hidden="true"></a>`;
   $tweet += `<a class="fa fa-steam" aria-hidden="true"></a>`;
@@ -57,22 +57,40 @@ const createTweetElement = (tweet) => {
   $tweet += `</footer>`;
   $tweet += `</article>`;
   return $tweet;
+};
+
+const renderTweets = (tweetData) => {
+  for(const tweet of tweetData) {
+    $('.all-tweets').append(createTweetElement(tweet));
+  }
 }
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 $(document).ready(() => {
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('.all-tweets').append($tweet);
+  renderTweets(data);
 });
