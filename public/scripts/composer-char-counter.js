bbe69b0b3dbd35606ef8740406beca2af8ff3e3c
Dan Pletzke <dpletzke@gmail.com>
2020-10-09 04:20:23 +0000

    eslint auto and manual fix

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
-  $('#tweet-text').on('input', function (event) {
+  $('#tweet-text').on('input', function() {
     const counter = $(this).siblings('div').children('output');
     counter.text(140 - this.value.length);
-    if(Number(counter.text()) < 0) {
+    if (Number(counter.text()) < 0) {
       counter.addClass('counter-negative');
     } else {
       counter.removeClass('counter-negative');
