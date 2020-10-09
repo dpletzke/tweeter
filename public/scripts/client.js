8c3e3f60940b8000a3ceb2cb59ec7b5c75e1d5ef
Dan Pletzke <dpletzke@gmail.com>
2020-10-09 04:18:29 +0000

    clear text box after submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -102,24 +102,28 @@ $(document).ready(function() {
   const $form = $('.new-tweet form');
   $form.submit(function(event) {
     event.preventDefault();
+
     const $tweetText = $('.new-tweet textarea').val();
     if(!$tweetText.length) {
       $errorLabel.hide();
       $errorLabel.text('❕Your text box is empty! Input is required.');
       $errorLabel.slideDown();
       return false;
+
     } else if($tweetText.length > 140) {
       $errorLabel.hide();
       $errorLabel.text('❕Your tweet is longer than 140 characters! Shorter message required.');
       $errorLabel.slideDown();
       return false;
     } 
+    
     $.ajax({ 
       url: '/tweets/', 
       method: 'POST', 
       data: $('.new-tweet textarea').serialize()
     })
     .then(function () {
+      $('.new-tweet textarea').val('');
       loadNewestTweet();
     });
 
